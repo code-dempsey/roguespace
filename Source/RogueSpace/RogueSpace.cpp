ebe33fe18f623a7c6fe7570dcf4b11f6151708c5
Noble <152928707+code-dempsey@users.noreply.github.com>
2026-08-17 23:30:57 -0400

    Init commit

---
+++ Source/RogueSpace/RogueSpace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RogueSpace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RogueSpace, "RogueSpace" );
